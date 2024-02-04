ec460885e4cea0075417a5d34df96d651f0bfd21
SMOLM <michal.smola007@gmail.com>
2024-02-04 18:03:32 +0100

    Dodano quiz chemiczny

--- subpages/chem/chem-script.js
+++ subpages/chem/chem-script.js
@@ -3,8 +3,9 @@ function getRandomInt(max) {
   }
 
 let questions= [
-    ["Auto przejechało 15km w 12 minut. Z jaką prętkością jechało? (odpowiedź z jednostką bez odstępu od numeru)", "75km/h"],
-    ["Sportowiec biegnie 10km/h. W ile przebigenie 3km? (odpowiedz podaj z jednistką bez odstępu od numeru)", "18min"],
+    ["Cu", "Miedź"],
+    ["Srebro", "Ag"],
+	["Au", "Złoto"],
 ];
 
 const quest1 = document.getElementById("questionText1"); 
@@ -27,8 +28,8 @@ sumbit1.addEventListener("click", () => {
 });
 
 let questions2= [
-	["Masz urządzenie elektryczne o napięciu U równym 230 V i prądzie I wynoszącym 5 A. Oblicz moc elektryczną P.", "1150W"],
-	["Masz obwód elektryczny z napięciem U równym 12 V i mocą P równą 48 W. Oblicz opór elektryczny R", "3ohmy"],
+	["Oblicz mase molową wody - H<sub>2</sub>O (Odpowiedź podaj z jednostką bez odstępu od liczby)", "18g/mol"],
+	["Oblicz mase molową dwutlenku węgla - CO<sub>2</sub> (Odpowiedź podaj z jednostką bez odstępu od liczby)", "44g/mol"],
 ];
 
 const quest2 = document.getElementById("questionText2"); 
@@ -50,8 +51,8 @@ sumbit2.addEventListener("click", () => {
 });
 
 let questions3= [
-    ["Oblicz przyspieszenie na powierzchni planety o masie 5.97 * 10<sup>24</sup>kg i promieniu 6.371 * 10<sup>6</sup>m.", "9.81m/s^2"],
-    ["Oblicz energię potencjalną obiektu o masie 100kg na 5km nad powierzchnią planety. (Odpowiedź podaj w przybliżeniu)", "4.905 * 10^6J"],
+    ["HCl", "Kwas solny"],
+    ["H₂SO₄", "Kwas sierkowy VI"],
 ];
 
 
